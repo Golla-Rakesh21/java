5ddd80317c6e7173f59f80d6e8412ddd0065cdd5
Golla-Rakesh21 <164481514+Golla-Rakesh21@users.noreply.github.com>
2024-07-23 22:38:55 +0530

    Create hello-world.java

---
+++ hello-world.java
@@ -0,0 +1,22 @@
+//method1
+
+package helloworld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
+
+//method2
+
+
+package helloworld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s="Hello";
+        String v=" world!";
+        System.out.println(s+v);
+    }
+    
+}
