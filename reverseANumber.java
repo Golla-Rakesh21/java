ea95c53eb47ee7816ccd037a63ac2dc7e75002d0
Golla Rakesh <rakeshgolla21@gmail.com>
2024-08-03 21:52:46 +0530

    Create reverseANumber.java

---
+++ reverseANumber.java
@@ -0,0 +1,22 @@
+//reverse of the given number
+package reverseofanumber;
+import java.util.*;
+public class ReverseOfANumber {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num;
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number u want to reverse : ");
+        num=sc.nextInt();
+        int temp=num;
+        while(num>0){
+            int r=num%10;
+            sum=(sum*10)+r;
+            num=num/10;
+        }
+        System.out.println(sum+ " is reverse of " + temp);
+        
+    }
+    
+}
