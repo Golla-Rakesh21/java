265ea2f4cf6ae2f908db025f6447dc8d99fbb9c1
Golla Rakesh <rakeshgolla21@gmail.com>
2024-08-03 21:52:04 +0530

    Create ARMSTRONG-NUMBER.java

---
+++ ARMSTRONG-NUMBER.java
@@ -0,0 +1,25 @@
+//armstrong Number checker
+package armstrongnumber;
+import java.util.*;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num,sum=0,r,temp;
+        Scanner sc=new Scanner(System.in);
+         System.out.print("Enter number to check : ");
+        num=sc.nextInt();
+        temp=num;
+        while(num>0){
+            r=num%10;
+            sum=sum+(r*r*r);
+            num=num/10;
+        }
+        if(temp==sum){
+            System.out.println("armstrong number");
+        }
+        else{
+             System.out.println("not armstrong number");
+        }
+    }
+    
+}
